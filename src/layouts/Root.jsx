bd46c77ca08e693ded340a3130e2723adfcb96e0
saiful693 <bdsaiful770@gamil.com>
2024-07-03 22:28:55 +0600

    My list Page done with update and delete functionality

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar";
+import Footer from "../shared/Footer";
 
 
 const Root = () => {
@@ -9,6 +10,7 @@ const Root = () => {
                 <Navbar></Navbar>
                 <Outlet></Outlet>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
